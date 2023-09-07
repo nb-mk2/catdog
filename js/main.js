6548e0c7a94af1c8b49b2a14dfe5d7e60fea075b
nb.mk2 <nicobaudis.net@gmail.com>
2023-09-07 20:12:03 -0300

    versio 1.0.0

---
+++ js/main.js
@@ -0,0 +1,89 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    $('.nosotros-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#nosotros').offset().top
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    $('.productos-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#productos').offset().top
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    $('.opiniones-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#opiniones').offset().top
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Date and time picker
+    $('#date').datetimepicker({
+        format: 'L'
+    });
+    $('#time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 2000,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
